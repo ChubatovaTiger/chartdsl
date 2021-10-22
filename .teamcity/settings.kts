84a2d4b697cba5823b730dfaf4d8a1cf54307ee5
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-10-22 17:21:33 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,9 +5,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 
 fun ProjectFeatures.addGraphs(buildTypeList : List<BuildType>) {
-
-    buildTypeList.forEach {
-
         
 
     feature {
