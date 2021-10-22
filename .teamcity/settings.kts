a9cb309c5ccfd0a5595936cad0a8076acbcb8793
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-10-22 16:47:50 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -79,8 +79,8 @@ fun ProjectFeatures.addGraphs(buildTypeList : List<BuildType>) {
                     [
                       {
                         "type": "valueTypes",
-                        "pattern": "buildStageDuration:*",
-                        "title": "Stage: {1}"
+                        "key": "buildStageDuration:buildStepRUNNER_4",
+                        "title": "my custom titel"
                       }
                     ]
             """.trimIndent()
