38f0bb5da6fee59e16e105a45f1480d8e0b5e699
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-10-25 14:04:00 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,7 @@
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2021.2.*
 
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2021.2.buildSteps.script
 
 
 
